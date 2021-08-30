da638d584217849d462f474789289af580d37599
Christopher Slinker <christopher.slinker@gmail.com>
2021-08-30 11:01:13 -0600

    Testing Pushing game and opening on laptop

--- SpaceRace/Assets/Scripts/PlayerController.cs
+++ SpaceRace/Assets/Scripts/PlayerController.cs
@@ -2,38 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
+
     private float verticalInput;
 
     private float speed = 5.0f;
 
     private float xRange = 6;
-// Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         
     }
-    
 
-// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
+                if (transform.position.x < -xRange)
+                {
+                    transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+                }
         
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+                if (transform.position.x > xRange)
+                {
+                    transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+                }
+                
+                horizontalInput = Input.GetAxis("Horizontal");
+                transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+                
+                verticalInput = Input.GetAxis("Vertical");
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
     }
 }
