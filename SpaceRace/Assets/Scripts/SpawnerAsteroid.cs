a2daf7bcd5ce9a16866dfaa0b4f089d96ef95083
Christopher Slinker <christopher.slinker@gmail.com>
2021-09-08 13:46:47 -0600

    Trying to fix asteroid spawner, still not working.
    Trying to fix my asteroid spawner so it's got a random spawn range, but I made it worse.

---
+++ SpaceRace/Assets/Scripts/SpawnerAsteroid.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerAsteroid : MonoBehaviour
+{
+    public GameObject[] asteroidPrefab;
+
+    private float spawnRangeX = 10;
+
+    private float SpawnPosZ = 20;
+
+    private float startDelay = 2;
+
+    private float startInterval = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAsteroid", startDelay, startInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 SpawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, SpawnPosZ);
+                
+        int asteroidIndex = Random.Range(0, asteroidPrefab.Length);
+                
+        Instantiate(asteroidPrefab [asteroidIndex], SpawnPos,
+            asteroidPrefab [asteroidIndex].transform.rotation); 
+    }
+}
