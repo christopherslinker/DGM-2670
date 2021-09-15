46619fff40a86ad4641ffd4f15bdb7e96c8706a4
Christopher Slinker <christopher.slinker@gmail.com>
2021-09-15 11:11:27 -0600

    Weapons and Collection ScriptableObjects

---
+++ PracticeProject/Assets/Scripts/CollectionsSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class CollectionsSO : ScriptableObject
+{
+    public List<WeaponSO> collection;
+    
+}
