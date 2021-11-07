e34d7ec29b4bbcc534c6020c01efe8357ddcfa0f
Christopher Slinker <christopher.slinker@gmail.com>
2021-11-06 18:35:40 -0700

    Added sources
    Added sources to my code

---
+++ SpaceRace/Assets/Scripts/Sources.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sources : MonoBehaviour
+{
+/*
+ https://learn.unity.com/course/create-with-code
+ https://www.youtube.com/watch?v=zc8ac_qUXQY
+ 
+ */
+}
